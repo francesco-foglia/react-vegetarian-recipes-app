a108fd37185410c1ea1eeead9f778d1cc3c4d6f5
Francesco Foglia <francescofogliajj@gmail.com>
2024-03-04 11:56:25 +0100

    Add a catch-all route to redirect any unmatched paths to the home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import Home from './components/Home';
@@ -12,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/recipe/:id" element={<RecipeDetail />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </Provider>
