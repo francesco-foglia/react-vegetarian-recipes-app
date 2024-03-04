3cc0fd5f9469b6a7224c330baccb597ae23250b8
Francesco Foglia <francescofogliajj@gmail.com>
2024-03-04 11:56:25 +0100

    Add a loading spinner to the center of the screen

---
+++ src/components/Spinner.js
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-white">
+      <img src="https://cdn.pixabay.com/animation/2022/07/29/03/42/03-42-15-221_512.gif" alt="spinner" width={100} height={100} className="w-10 h-10" />
+    </div>
+  );
+}
