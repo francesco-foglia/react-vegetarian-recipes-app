29fc0ded528da77d76dc40f408f87110b3640d47
Francesco Foglia <francescofogliajj@gmail.com>
2024-05-29 16:08:23 +0200

    modified:   src/components/RecipeList.tsx

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -21,7 +21,7 @@ const RecipeList: React.FC<Props> = ({ recipes, query, currentPage }) => {
       {recipes.map((recipe) => (
         <Link key={recipe.id} to={`/recipe/${recipe.id}?query=${query}&page=${currentPage}`} className="flex">
           <div
-            className="w-full p-[4px] bg-white rounded-[20px] flex flex-col justify-start items-start group"
+            className="w-full p-[4px] bg-white rounded-[20px] flex flex-col justify-start items-center group"
             style={{ boxShadow: "0px 6px 12px #00000019" }}
           >
             <div className="w-full h-auto min-h-[250px] max-h-[250px]">
@@ -35,7 +35,7 @@ const RecipeList: React.FC<Props> = ({ recipes, query, currentPage }) => {
                 className="w-full h-auto object-cover object-center rounded-[16px] min-h-[250px] max-h-[250px]"
               />
             </div>
-            <h2 className="p-2 pt-[12px] group-hover:text-green-500 transition-all duration-300 ease-in-out text-black text-left">
+            <h2 className="p-2 pt-[12px] group-hover:text-green-500 transition-all duration-300 ease-in-out text-black text-center">
               {recipe.title}
             </h2>
           </div>
