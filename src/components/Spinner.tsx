047f0d158eadbb5dbb6b2ae791521c150c67cdfe
Francesco Foglia <francescofogliajj@gmail.com>
2024-05-29 16:08:23 +0200

    new file:   src/components/Spinner.tsx

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="fixed top-0 left-0 z-[1] w-full h-screen bg-[#242424] flex justify-center items-center">
+      <span className="loader"></span>
+    </div>
+  );
+}
