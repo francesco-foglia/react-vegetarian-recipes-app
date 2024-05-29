9e1c702b064ed31a207d78b9c8125be7bca6e682
Francesco Foglia <francescofogliajj@gmail.com>
2024-05-29 16:08:22 +0200

    new file:   src/components/Home.tsx

---
+++ src/components/Home.tsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { Helmet } from "react-helmet";
+
+import SearchBar from './SearchBar';
+import RecipeList from './RecipeList';
+import Pagination from './Pagination';
+import ErrorMessage from './ErrorMessage';
+import Spinner from './Spinner';
+
+interface Recipe {
+  id: string;
+  image: string;
+  title: string;
+}
+
+const Home: React.FC = () => {
+  const [searchParams] = useSearchParams();
+
+  const [query, setQuery] = useState<string>(
+    searchParams.get("query") || ''
+  );
+
+  const [value, setValue] = useState<string>(
+    searchParams.get("query") || ''
+  );
+
+  const [currentPage, setCurrentPage] = useState<number>(
+    parseInt(searchParams.get("page") || '1', 10)
+  );
+
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  const [totalResults, setTotalResults] = useState<number>(0);
+  const [spinner, setSpinner] = useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const itemsPerPage = 12;
+  const navigate = useNavigate();
+
+  const apiKey: string = import.meta.env.VITE_APP_API_KEY;
+
+  const fetchRecipes = useCallback(async () => {
+    setSpinner(true);
+    try {
+      const response = await axios.get(
+        `https://api.spoonacular.com/recipes/complexSearch?query=${query}&diet=vegetarian&number=${itemsPerPage}&apiKey=${apiKey}&offset=${(currentPage - 1) * itemsPerPage}`
+      );
+      setRecipes(response.data.results);
+      setTotalResults(response.data.totalResults);
+      setSpinner(false);
+    } catch (error) {
+      setErrorMessage('An error occurred while fetching recipes. Please try again later.');
+      setSpinner(false);
+      console.error(error);
+    }
+  }, [query, currentPage, apiKey]);
+
+  useEffect(() => {
+    fetchRecipes();
+  }, [fetchRecipes]);
+
+  if (spinner) return <Spinner />;
+
+  return (
+    <>
+      <Helmet>
+        <title>Vegetarian Recipes</title>
+      </Helmet>
+      <div className="w-full min-h-screen py-10 px-[5%] 2xl:container mx-auto">
+        <div className="flex justify-center items-center mb-10">
+          <h1 className="text-3xl cursor-pointer" onClick={() => {
+            setQuery('');
+            setValue('');
+            setCurrentPage(1);
+            navigate('/')
+          }}>
+            Vegetarian Recipes
+          </h1>
+        </div>
+
+        {!errorMessage && (
+          <main>
+            <SearchBar
+              // fetchRecipes={fetchRecipes}
+              value={value}
+              setValue={setValue}
+              setQuery={setQuery}
+              setCurrentPage={setCurrentPage}
+            />
+            {recipes.length > 0 ? (
+              <>
+                <RecipeList
+                  recipes={recipes}
+                  query={query}
+                  currentPage={currentPage}
+                />
+                {totalResults > itemsPerPage && (
+                  <Pagination
+                    query={query}
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                    totalResults={totalResults}
+                    itemsPerPage={itemsPerPage}
+                  />
+                )}
+              </>
+            ) : (
+              <p className="text-center my-5">No recipes found. Try another search.</p>
+            )}
+          </main>
+        )}
+
+        {errorMessage && <ErrorMessage errorMessage={errorMessage} />}
+      </div>
+    </>
+  );
+};
+
+export default Home;
